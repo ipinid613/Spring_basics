a3613d47eb7b859d712fee32a71dabf85e08e4a7
ipinid613 <ipinid613@kakao.com>
2021-07-01 04:55:33 +0900

    21.06.30 세부페이지 연결 추가

--- Cheers/src/main/java/com/sparta/w1homework/controller/CheerController.java
+++ Cheers/src/main/java/com/sparta/w1homework/controller/CheerController.java
@@ -1,10 +1,12 @@
 package com.sparta.w1homework.controller;
 
 import com.sparta.w1homework.domain.Cheer;
+import com.sparta.w1homework.domain.CheerDetailResponse;
 import com.sparta.w1homework.domain.CheerRepository;
 import com.sparta.w1homework.domain.CheerRequestDto;
 import com.sparta.w1homework.service.CheerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -39,5 +41,15 @@ public class CheerController {
     public Long updateCheer(@PathVariable Long id, @RequestBody CheerRequestDto requestDto) {
         cheerService.update(id, requestDto);
         return id;
+    }
+
+    @GetMapping("/api/cheers/{id}")
+    //사용자의 HttpRequest에 대한 응답 데이터를 포함하는 클래스 = ResponseEntity
+    //detail.js의 function getPostDetail이 실행되어서
+    // /api/cheers/{id}로 get요청이 오면, id값을 받아서 findById라는 이름의 메소드 실행할건데,
+    // 이 메소드는 <CheerDetailResponse>에 작성자, 시간 등 사용자가 웹에서 입력한 정보를 담고, 이를 가지고 cheerService의 findByID를 실행!
+    // cheerService.findById를 실행해서 반환받은 결과를 ajax통신을 통해 JSON형식으로 detail.js의 function getPostDetail에 넘겨줌!!
+    public ResponseEntity<CheerDetailResponse> findById(@PathVariable Long id ) {
+        return ResponseEntity.ok(cheerService.findById(id));
     }
 }
--- Cheers/src/main/java/com/sparta/w1homework/service/CheerService.java
+++ Cheers/src/main/java/com/sparta/w1homework/service/CheerService.java
@@ -1,6 +1,7 @@
 package com.sparta.w1homework.service;
 
 import com.sparta.w1homework.domain.Cheer;
+import com.sparta.w1homework.domain.CheerDetailResponse;
 import com.sparta.w1homework.domain.CheerRepository;
 import com.sparta.w1homework.domain.CheerRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -20,5 +21,15 @@ public class CheerService {
         );
         cheer.update(requestDto);
         return cheer.getId();
+    }
+
+    //CheerController에서 cheerService.findById 실행 명령이 왔으므로 아래를 실행
+    //cheerService.findById는 cheerRepository.findById 실행 결과를 반환함.
+    //해당 id를 가진 게시물이 있다면 해당 게시물의 전체 정보를 반환해줌!!
+    @Transactional
+    public CheerDetailResponse findById(Long id) {
+        return cheerRepository.findById(id)
+                .map(CheerDetailResponse::of)
+                .orElseThrow(()-> new IllegalArgumentException("id를 찾지 못했습니다."));
     }
 }
